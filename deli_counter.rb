a5a1dad79ca6febee6ac77b238db6e43fafabc0e
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-28 11:04:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ another_deli = []
 
 def line(deli)
     #puts "The line is currently empty."
-    new_array = [str.push]
+    new_array = []
   if deli.length === 3
     str = ''
     deli.each_with_index do | name, i |
