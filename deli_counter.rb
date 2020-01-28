c0a15e0273b75b6f8f3cac26a63ed86fed989c29
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-28 10:27:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+katz_deli = []
+other_deli = []
+another_deli = []
+
+def line(katz_deli)
+    puts "The line is currently empty."
+  #if other_deli
+    #puts "The line is currently: #{other_deli}"
+  #if another_deli
+    #puts "The line is currently: #{another_deli}"
+  #else
+    #puts "The line is currently empty."
+  #end
+end
