57a218efd1c2e9f966efde6aa85a76071f11091e
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-28 10:57:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,13 @@ another_deli = []
 
 def line(deli)
     #puts "The line is currently empty."
-  if deli.each(index) === 3
-    puts "The line is currently: #{deli}"
+  if deli.length === 3
+    str = ''
+    deli.each_with_index do | name, i |
+      str += "#{i}. #{name}"
+    end
+    
+    puts "The line is currently: #{str}"
   #if another_deli
     #puts "The line is currently: #{another_deli}"
   else
