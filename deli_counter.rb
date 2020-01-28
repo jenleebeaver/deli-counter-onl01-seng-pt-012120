c4360c98770c7f975d1a0a5739a2d1ae56331850
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-28 10:51:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,8 +7,8 @@ def line(katz_deli)
     #puts "The line is currently empty."
   if other_deli.length
     puts "The line is currently: #{other_deli}"
-  if another_deli
-    puts "The line is currently: #{another_deli}"
+  #if another_deli
+    #puts "The line is currently: #{another_deli}"
   else
     puts "The line is currently empty."
   end
