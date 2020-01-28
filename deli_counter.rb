9b6da5642cca80b90644a8fbe1469963dbe9395b
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-28 12:00:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ end
 
 def now_serving(deli)
   #line = deli.shift(name)
-  if deli.length > 0 
+  if deli.length > 0
   puts "Currently serving #{deli[0]}."
 else
   puts "There is nobody waiting to be served!"
